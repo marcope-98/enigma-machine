77c6d4803195f4c0b65b07cdd67bcc41342580d9
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-02-01 14:32:02 +0100

    [enmach/utils] Removes unused reverse_tuple implementation

--- include/enmach/utils.hpp
+++ include/enmach/utils.hpp
@@ -7,19 +7,6 @@
 
 namespace enmach
 {
-  template<class Tuple, std::size_t... Is>
-  constexpr auto reverse_tuple_impl(Tuple &&t, std::index_sequence<Is...>)
-  {
-    return std::make_tuple(std::get<sizeof...(Is) - 1 - Is>(std::forward<Tuple>(t))...);
-  }
-
-  template<class Tuple>
-  constexpr auto reverse_tuple(Tuple &&t)
-  {
-    constexpr std::size_t N = std::tuple_size<std::decay_t<Tuple>>::value;
-    return reverse_tuple_impl(std::forward<Tuple>(t), std::make_index_sequence<N>{});
-  }
-
   template<class...>
   inline constexpr auto is_unique = std::true_type{};
 
