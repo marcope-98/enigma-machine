f74c4de40c28d197e02265d8e95027871b28598e
marcope-98 <marco.eugenio.peressutti@gmail.com>
2026-02-28 11:13:26 +0100

    [Issue #4] EnigmaM4 uses std::enable_if_t as alias, producing unreadable errors on constraint violation

--- include/enmach/enmach.hpp
+++ include/enmach/enmach.hpp
@@ -27,15 +27,20 @@ namespace enmach
       Args...>;
 
   template<class Plugboard, class Reflector, class Zusatzwalze, class... Rotors>
-  using EnigmaM4 = std::enable_if_t<
-      (std::is_same_v<Zusatzwalze, BETA> || std::is_same_v<Zusatzwalze, GAMMA>) && /* Zusatzwalze must be either gamma or beta*/
-          !(Set<BETA, GAMMA>::template is_in_set<Rotors>() || ...) /* Other Rotors must not be of type gamma or beta */,
-      EnigmaMachine<
-          EnigmaMachineConfiguration<
-              Set<I, II, III, IV, V, VI, VII, VIII, BETA, GAMMA>,
-              Set<ukw::ThinB, ukw::ThinC>,
-              4>,
-          Plugboard, Reflector, Zusatzwalze, Rotors...>>;
+  struct EnigmaM4_t
+  {
+    static_assert(std::is_same_v<Zusatzwalze, BETA> || std::is_same_v<Zusatzwalze, GAMMA>, "[ERROR] Zusatzwalze (4th rotor) must be either rotor_tags::BETA or rotor_tags::GAMMA.");
+    static_assert(!(Set<BETA, GAMMA>::template is_in_set<Rotors>() || ...), "the three standard rotors must not be BETA or GAMMA. Those are reserved for the Zusatzwalze position.");
+    using type = EnigmaMachine<
+        EnigmaMachineConfiguration<
+            Set<I, II, III, IV, V, VI, VII, VIII, BETA, GAMMA>,
+            Set<ukw::ThinB, ukw::ThinC>,
+            4>,
+        Plugboard, Reflector, Zusatzwalze, Rotors...>;
+  };
+
+  template<class Plugboard, class Reflector, class Zusatzwalze, class... Rotors>
+  using EnigmaM4 = typename EnigmaM4_t<Plugboard, Reflector, Zusatzwalze, Rotors...>::type;
 } // namespace enmach
 
 #endif // ENMACH_ENMACH_HPP_
